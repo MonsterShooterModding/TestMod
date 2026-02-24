3772e676ba5bfb7c9f99c657a34ac62f44136368
Ivbermaster <ivbermaster@gmail.com>
2026-02-24 20:10:37 +0100

    Initial commit

---
+++ testmod/TestModPlugin.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("test.testmod", "Test Mod", "1.0.0")]
+public class TestModPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("TestMod: Awake called (loaded successfully)");
+    }
+}
